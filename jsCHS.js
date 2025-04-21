faab21f498340765ae820d460ceca770b66770dc
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-04-21 16:25:03 +0700

    first commit

---
+++ jsCHS.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Red Velvet")
+})
+
+Audio.addEventListener("click", function Audio(){
+    alert("Ini Audio Red Velvet")
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Red Velvet")
+})
